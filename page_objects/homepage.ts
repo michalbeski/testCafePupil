520c4ab11b22e90d1a27b1e3e0a87d51ac20bf93
Michal Beski <michal.beski@avanade.com>
2019-01-17 14:05:31 +0100

    dodane testy

--- page_objects/homepage.ts
+++ page_objects/homepage.ts
@@ -9,19 +9,37 @@ export class Homepage extends Page {
         Homepage.currentDate= new Date().toLocaleDateString();
     }
 
-    public mainTab = Selector('a.navbar-brand navbar-left').withAttribute('href', '.*page-top*');
-    //public mainTab = Selector('.a').withText('Sklep dla zwierzaka');
+    //header
+    public mainTab = Selector('.navbar-brand.navbar-left').withAttribute('href', '#page-top');
     public navBar = Selector('.nav navbar-nav navbar-right');
-    //public galeriaTab = Selector('a').withAttribute('href', '.*photo.*');
-    //public galeriaTab = Selector('.page-scroll').child('a').withAttribute('href', '.*photo.*');
-    public galeriaTab = Selector('a').withText('Galeria');
+    public galeriaTab = Selector('#bs-example-navbar-collapse-1 a').withAttribute('href', '#photo');
     public createButton = Selector("button").withText("Create");
-    public oSklepieTab = Selector('a').withAttribute('href', '.*about.*');
-    public kontakTab = Selector('a').withAttribute('href', '.*contact.*');
-    public lokalizacjaTab = Selector('a').withAttribute('href', '.*localization.*'); 
+    public oSklepieTab = Selector('#bs-example-navbar-collapse-1 a').withAttribute('href', '#about');
+    public kontakTab = Selector('#bs-example-navbar-collapse-1 a').withAttribute('href', '#contact');
+    public lokalizacjaTab = Selector('#bs-example-navbar-collapse-1 a').withAttribute('href', '#localization'); 
+    public introText = Selector('.intro-text > span.name');
+    public logoPupil = Selector('.img-rounded.img-responsive').withAttribute('src', 'img/logo_pupil.jpg');
 
+    //galeria section
+    public galeriaSection = Selector('#photo');
+    public firstPhoto = Selector('#photo .img-responsive').withAttribute('src', 'img/1.JPG');
 
+    //o sklepie section
+    public oSklepieSection = Selector('#about');
+    public phoneNumber = Selector('#about .container');
+    
+    //kontakt section
+    public kontaktSection = Selector('#about');
+    public contactForm = Selector('#contactForm');
 
+    //lokalizacj section
+    public lokalizacjaSection = Selector('#about');
+    public googleMap = Selector('#GoogleMap');
+
+    //footer
+    public footerSection = Selector('div.footer-below > .container');
+    public facebookLink = Selector('.footer-below .list-inline a').withAttribute('href', 'https://www.facebook.com/SklepPupilZarkiLetnisko');
+    public facebookPupilPage = Selector('#js_5q');
 
 async goToMainTab() {
     await t 
--- tests/SmokeTest.ts
+++ tests/SmokeTest.ts
@@ -1,4 +1,5 @@
 import { Homepage } from '../page_objects/homepage';
+import { ClientFunction } from 'testcafe';
 
 const homepage = new Homepage;
 
@@ -7,18 +8,61 @@ fixture `SmokeTest`.beforeEach(async t=> {
 });
     
     test ("Test navigation bar", async (t) => {
-       // await homepage.goToMainTab();
-        console.log('--------> PASSSSSSSSSS')
         await t
-            //.click(homepage.mainTab)
-        //     .debug();
-        //     const count = await (homepage.galeriaTab).filter(node => { debugger; return true;}).count;
-        //  await t    
-        //     .click(homepage.galeriaTab)
-        //     .click(homepage.oSklepieTab)
-        //     .click(homepage.kontakTab)
-        //     .click(homepage.lokalizacjaTab)
-});
+        .click(homepage.mainTab)
+        .expect(homepage.mainTab.textContent).eql('Sklep dla zwierzaka', 'Read value not equal to given name')
+        .expect(homepage.galeriaTab.textContent).eql('Galeria', 'Read value not equal to given name')
+        .expect(homepage.oSklepieTab.textContent).eql('O sklepie', 'Read value not equal to given name')
+        .expect(homepage.kontakTab.textContent).eql('Kontakt', 'Read value not equal to given name')
+        .expect(homepage.lokalizacjaTab.textContent).eql('Lokalizacja', 'Read value not equal to given name')
+        .expect(homepage.logoPupil.visible).ok('Pupil logo not visible');
+    });
 
-
+    test ("Test 'Galeria' section", async (t) => {
+        await t
+        .click(homepage.galeriaTab)
+        .expect(homepage.galeriaSection.visible).ok('Galeria section not visible')
+        .expect(homepage.firstPhoto.visible).ok('First photo in galery section not visible');
+    });
         
+
+    test ("Test 'O Sklepie' section", async (t) => {
+        await t
+        .click(homepage.oSklepieTab)
+        .expect(homepage.oSklepieSection.visible).ok('O Sklepie section not visible')
+        .expect(homepage.phoneNumber.textContent).contains('666 926 419' ,'Phone number in "O Sklepie" section not visible');
+    });
+
+
+    test ("Test 'Kontakt' section", async (t) => {
+        await t
+        .click(homepage.kontakTab)
+        .expect(homepage.kontaktSection.visible).ok('Kontakt section not visible')
+        .expect(homepage.phoneNumber.visible).ok('Contact Form in "Kontakt" section not visible');
+    });
+
+    test ("Test 'Lokalizacja' section", async (t) => {
+        await t
+        .click(homepage.lokalizacjaTab)
+        .expect(homepage.lokalizacjaSection.visible).ok('Lokalizacja section not visible')
+        .expect(homepage.googleMap.visible).ok('Google Map in "Lokalizacja" section not visible');
+    });
+    
+    test ("Test 'footer' section", async (t) => {
+        await t
+        .click(homepage.lokalizacjaTab)
+        .expect(homepage.footerSection.visible).ok('Footer section not visible')
+    });
+
+    test ("Test 'Facebook' redirection", async (t) => {
+      
+        const getLocation = ClientFunction(() => document.location.href);
+    
+        await t
+         .expect(homepage.facebookLink.visible).ok('Facebook link not visible')
+         .click(homepage.facebookLink)
+         //.debug()
+         const facebookPupilPageElement = await homepage.facebookPupilPage.with({ visibilityCheck: true })();
+        await t 
+         .expect(getLocation()).contains('www.facebook.com/SklepPupilZarkiLetnisko', 'URL is invalid');
+    });
